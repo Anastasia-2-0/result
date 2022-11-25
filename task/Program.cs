3ca5cad0b397a2fbc6198d37cfdc73e04ca27f8b
Anastasia-2-0 <nosova0702@mail.ru>
2022-11-25 13:53:29 +0300

    entering an array

--- task/Program.cs
+++ task/Program.cs
@@ -1,2 +1,27 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Write("Введите количество элементов в массиве: ");
+int size = int.Parse(Console.ReadLine());
+
+string[] arrayFirst = new string[size];
+string[] arraySecond = new string[arrayFirst.Length];
+FillArray(arrayFirst);
+Console.Write("Ваш массив: ");
+PrintArray(arrayFirst);
+
+void FillArray(string[] array)
+{
+    for (int i = 0; i < size; i++)
+    {
+        Console.WriteLine($"Введите {i + 1} элемент: ");
+        string element = Convert.ToString(Console.ReadLine());
+        array[i] = element;
+    }
+}
+
+void PrintArray(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write(array[i] + " ");
+    }
+    Console.WriteLine();
+}
